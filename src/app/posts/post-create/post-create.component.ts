341a16f174802058389666aca286156943cf57ac
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-07-24 20:51:51 +0800

    l-26

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(postForm: NgForm) {
+    if (postForm.invalid) {
+      return;
+    }
+    // const post: Post = {
+    //   title: postForm.value.title,
+    //   content: postForm.value.content
+    // };
+    // this.postCreated.emit(post);
+    this.postsService.addPost(postForm.value.title, postForm.value.content);
+  }
+}
