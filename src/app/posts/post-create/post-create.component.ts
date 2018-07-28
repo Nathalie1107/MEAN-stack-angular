1a68fba67a4067b8c75be90fb4268d608901465a
nathalie1107 <nathalie.nhu.y@gmail.com>
2018-07-28 16:16:50 +0800

    l-68 edit function + routing backend + update posts

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,28 +1,62 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import {
+  ActivatedRoute,
+  ParamMap
+} from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   @Output() postCreated = new EventEmitter<Post>();
 
-  constructor(public postsService: PostsService) {}
+  post: Post;
+  private mode = 'create';
+  private postId: string;
 
-  onAddPost(postForm: NgForm) {
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(postForm: NgForm) {
     if (postForm.invalid) {
       return;
     }
-    // const post: Post = {
-    //   title: postForm.value.title,
-    //   content: postForm.value.content
-    // };
-    // this.postCreated.emit(post);
-    this.postsService.addPost(postForm.value.title, postForm.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(postForm.value.title, postForm.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        postForm.value.title,
+        postForm.value.content
+      );
+    }
+
     postForm.resetForm();
   }
 }
